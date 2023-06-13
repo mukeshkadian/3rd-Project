e270286a8dbfb791a56beb1442ead72cde02054f
Mukesh Kadian <mukeshkadianarmy@gmail.com>
2023-06-13 09:33:16 -0400

    Initial Version

---
+++ src/test/java/com/example/demo/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Department;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+       @DataJpaTest
+       class DepartmentRepositoryTest {
+           @Autowired
+           private DepartmentRepository departmentRepository;
+
+           @Autowired
+           private TestEntityManager entityManager;
+
+           @BeforeEach
+           void setUp() {
+               Department department= Department.builder().departmentName("Mech Engineering")
+                       .departmentCode("Me-011").departmentAddress("Delhi").build();
+
+
+               entityManager.persist(department);
+    }
+         @Test
+        public void whenFindById_thenReturnDepartment()  {
+               Department department= departmentRepository.findById(1L).get();
+               assertEquals(department.getDepartmentName(), "Mech Engineering");
+        }
+}
